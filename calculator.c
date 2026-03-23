93886d267fa6eb01725c8563201c355c991b92c6
Codelady00 <hunarbhayana12@gmail.com>
2026-03-23 21:23:40 +0530

    power and modulus

--- calculator.c
+++ calculator.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main() {
+    FILE *fp;
     int choice;
     double a, b, result;
+
+    fp = fopen("history.txt", "a");
+
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
 
     do {
         printf("\n===== CALCULATOR MENU =====\n");
@@ -11,12 +20,14 @@ int main() {
         printf("2. Subtraction\n");
         printf("3. Multiplication\n");
         printf("4. Division\n");
-        printf("5. Exit\n");
+        printf("5. Power\n");
+        printf("6. Modulus\n");
+        printf("7. Exit\n");
 
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        if (choice >= 1 && choice <= 4) {
+        if (choice >= 1 && choice <= 5) {
             printf("Enter two numbers: ");
             scanf("%lf %lf", &a, &b);
         }
@@ -25,16 +36,19 @@ int main() {
             case 1:
                 result = a + b;
                 printf("Result = %.2lf\n", result);
+                fprintf(fp, "%.2lf + %.2lf = %.2lf\n", a, b, result);
                 break;
 
             case 2:
                 result = a - b;
                 printf("Result = %.2lf\n", result);
+                fprintf(fp, "%.2lf - %.2lf = %.2lf\n", a, b, result);
                 break;
 
             case 3:
                 result = a * b;
                 printf("Result = %.2lf\n", result);
+                fprintf(fp, "%.2lf * %.2lf = %.2lf\n", a, b, result);
                 break;
 
             case 4:
@@ -43,11 +57,24 @@ int main() {
                 } else {
                     result = a / b;
                     printf("Result = %.2lf\n", result);
+                    fprintf(fp, "%.2lf / %.2lf = %.2lf\n", a, b, result);
                 }
                 break;
 
-            case 5: 
-                printf("\nThank you for using the calculator!\n");
+            case 5:
+                result = pow(a, b);
+                printf("Result = %.2lf\n", result);
+                fprintf(fp, "%.2lf ^ %.2lf = %.2lf\n", a, b, result);
+                break;
+
+            case 6: {
+                printf("Result = %.2f\n", fmod(a, b));
+                fprintf(fp, "%.2lf %% %.2lf = %.2f\n", a, b, fmod(a, b));
+                break;
+            }
+
+            case 7:
+                printf("Thank you for using the calculator!\n");
                 printf("Exiting calculator...\n");
                 break;
 
@@ -55,7 +82,9 @@ int main() {
                 printf("Invalid choice! Try again.\n");
         }
 
-    } while (choice != 5);
+    } while (choice != 7);
+
+    fclose(fp);
 
     return 0;
 }
